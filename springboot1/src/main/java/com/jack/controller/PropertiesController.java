b90c05c7bfac41dfc1d5e54d34206dce0cfe6a36
jack <wj903829182@sina.cn>
2017-08-05 11:51:36 +0800

    src资源

--- springboot1/src/main/java/com/jack/controller/PropertiesController.java
+++ springboot1/src/main/java/com/jack/controller/PropertiesController.java
@@ -1,5 +1,7 @@
 package com.jack.controller;
 
+import com.jack.config.AuthorSetting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,19 @@ public class PropertiesController {
     private String bookAuthor;
     @Value("${book.name}")
     private String bookName;
+
+    @Autowired
+    private AuthorSetting authorSetting;
+
     @RequestMapping("/property")
     public String helloProperty(){
         System.out.println("helloProperty()");
         return "book name is :"+bookName+" and book author is :"+bookAuthor;
     }
+
+    @RequestMapping("/author")
+    public String author(){
+        return "name: "+authorSetting.getName()+",age: "+authorSetting.getAge();
+    }
+
 }
