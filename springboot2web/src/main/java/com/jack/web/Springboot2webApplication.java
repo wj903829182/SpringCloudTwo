7a3b79c220e7734a8867fa79c190452a14e114ee
jack <wj903829182@sina.cn>
2017-08-06 12:00:50 +0800

    springboot的web项目

---
+++ springboot2web/src/main/java/com/jack/web/Springboot2webApplication.java
@@ -0,0 +1,12 @@
+package com.jack.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2webApplication.class, args);
+	}
+}
---
+++ springboot2web/src/test/java/com/jack/web/Springboot2webApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2webApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
