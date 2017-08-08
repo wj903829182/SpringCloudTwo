605d9ba13e3c3520f45526a623cf891417464094
jack <wj903829182@sina.cn>
2017-08-08 22:28:24 +0800

    src资源

--- springboot2web/src/main/java/com/jack/web/controller/ThymeleafController.java
+++ springboot2web/src/main/java/com/jack/web/controller/ThymeleafController.java
@@ -1,22 +1,22 @@
 package com.jack.web.controller;
 
 import com.jack.web.pojo.Person;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class ThymeleafController {
     @RequestMapping("/thymeleaf")
     public String index(Model model){
         Person single = new Person("jack", 11);
         List<Person> people = new ArrayList<Person>();
         Person p1 = new Person("xx", 12);
-        Person p2 = new Person("xx", 13);
-        Person p3 = new Person("xx", 14);
+        Person p2 = new Person("yy", 13);
+        Person p3 = new Person("zz", 14);
         people.add(p1);
         people.add(p2);
         people.add(p3);
