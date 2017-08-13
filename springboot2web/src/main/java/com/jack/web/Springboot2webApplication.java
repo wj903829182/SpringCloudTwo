956f0805bc05ecf17cd5b2262b95b7f2e32245d5
jack <wj903829182@sina.cn>
2017-08-13 20:37:32 +0800

    src资源

--- springboot2web/src/main/java/com/jack/web/Springboot2webApplication.java
+++ springboot2web/src/main/java/com/jack/web/Springboot2webApplication.java
@@ -1,16 +1,14 @@
 package com.jack.web;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Springboot2webApplication {
@@ -19,4 +17,34 @@ public class Springboot2webApplication {
         SpringApplication.run(Springboot2webApplication.class, args);
     }
 
+  /* @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        //重写上下文设置，添加路径和安全集合
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        //创建一个httpconnector以被使用
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        //application.yml里面配置的端口
+        connector.setRedirectPort(9090);
+        return connector;
+    }
+*/
 }
