edc2ea916ea22a616c49cd2d904365c49c3472c5
jack <wj903829182@sina.cn>
2017-08-13 09:32:14 +0800

    src资源

--- springboot2web/src/main/java/com/jack/web/Springboot2webApplication.java
+++ springboot2web/src/main/java/com/jack/web/Springboot2webApplication.java
@@ -2,11 +2,21 @@ package com.jack.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Springboot2webApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot2webApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2webApplication.class, args);
+    }
+
 }
