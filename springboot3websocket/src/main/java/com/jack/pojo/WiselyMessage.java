e4b6b123c3b0d63cdcdcfb2deadda055131c6bb7
jack <wj903829182@sina.cn>
2017-08-14 22:44:43 +0800

    websocket

---
+++ springboot3websocket/src/main/java/com/jack/pojo/WiselyMessage.java
@@ -0,0 +1,16 @@
+package com.jack.pojo;
+
+/**
+ * 浏览器向服务端发送的消息用此类接收
+ */
+public class WiselyMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ springboot3websocket/src/main/java/com/jack/pojo/WiselyResponse.java
@@ -0,0 +1,20 @@
+package com.jack.pojo;
+
+/**
+ * 服务端向浏览器发送的此类的消息
+ */
+public class WiselyResponse {
+    private String responseMessage;
+
+    public WiselyResponse(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
