29abd80bdc2c40e06c9a38f3466205b43b168103
jack <wj903829182@sina.cn>
2017-08-14 22:07:26 +0800

    websocket

---
+++ springboot3websocket/src/main/java/com/jack/Springboot3websocketApplication.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3websocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3websocketApplication.class, args);
+	}
+}
