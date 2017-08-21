35f1b63e75ed7aadfabbf2ab3b82d1bec9793e15
jack <wj903829182@sina.cn>
2017-08-21 21:05:34 +0800

    websocket点对点

--- springboot3websocket/src/main/java/com/jack/Springboot3websocketApplication.java
+++ springboot3websocket/src/main/java/com/jack/Springboot3websocketApplication.java
@@ -2,21 +2,18 @@ package com.jack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-import java.util.List;
 
 @SpringBootApplication
 /**
  * 通过@EnableWebSocketMessageBroker注解开启使用STOMP协议来传输基于代理（message broker）的消息，这时控制器
  * 支持使用@MessageMapping，就像使用@RequestMapping一样
  */
-@EnableWebSocketMessageBroker
-public class Springboot3websocketApplication extends AbstractWebSocketMessageBrokerConfigurer{
+//@EnableWebSocketMessageBroker
+public class Springboot3websocketApplication /*extends AbstractWebSocketMessageBrokerConfigurer*/{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3websocketApplication.class, args);
@@ -27,19 +24,19 @@ public class Springboot3websocketApplication extends AbstractWebSocketMessageBro
 	 *
 	 * @param stompEndpointRegistry
 	 */
-	@Override
+	/*@Override
 	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
 		//注册一个STOMP的endpoint，并指定使用SockJS协议
 		stompEndpointRegistry.addEndpoint("/endpointWisely").withSockJS();
-	}
+	}*/
 
 	/**
 	 * 配置消息代理（MessageBroker）
 	 * @param registry
 	 */
-	@Override
+	/*@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		//广播式应配置一个/topic消息代理
 		registry.enableSimpleBroker("/topic");
-	}
+	}*/
 }
