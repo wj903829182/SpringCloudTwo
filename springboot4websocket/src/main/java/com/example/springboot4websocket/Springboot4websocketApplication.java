1bdd45da9bc7885edbb56b8cbba1608cf35067f7
jack <wj903829182@sina.cn>
2017-09-23 20:20:09 +0800

    web

---
+++ springboot4websocket/src/main/java/com/example/springboot4websocket/Springboot4websocketApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot4websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4websocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4websocketApplication.class, args);
+	}
+}
