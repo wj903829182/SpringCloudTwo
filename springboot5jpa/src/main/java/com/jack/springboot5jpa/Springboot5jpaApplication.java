57641e3ffb836467f16f68bcb24011b5b83367e1
jack <wj903829182@sina.cn>
2017-10-03 08:38:12 +0800

    spring boot JPA

---
+++ springboot5jpa/src/main/java/com/jack/springboot5jpa/Springboot5jpaApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot5jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot5jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot5jpaApplication.class, args);
+	}
+}
---
+++ springboot5jpa/src/test/java/com/jack/springboot5jpa/Springboot5jpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.springboot5jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot5jpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
