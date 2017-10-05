c1d0b8adddc93d58dc8c082a82dd1db48de1c45d
jack <wj903829182@sina.cn>
2017-10-05 11:21:59 +0800

    web

---
+++ springboot7transaction/src/main/java/com/example/springboot7transaction/Springboot7transactionApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot7transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot7transactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot7transactionApplication.class, args);
+	}
+}
---
+++ springboot7transaction/src/test/java/com/example/springboot7transaction/Springboot7transactionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot7transaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot7transactionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
