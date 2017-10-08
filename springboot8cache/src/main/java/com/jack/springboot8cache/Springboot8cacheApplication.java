b89cca453c127a733f2a1bbf109bf4059c8b9ab1
jack <wj903829182@sina.cn>
2017-10-08 22:21:30 +0800

    spring boot cache

---
+++ springboot8cache/src/main/java/com/jack/springboot8cache/Springboot8cacheApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot8cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot8cacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot8cacheApplication.class, args);
+	}
+}
