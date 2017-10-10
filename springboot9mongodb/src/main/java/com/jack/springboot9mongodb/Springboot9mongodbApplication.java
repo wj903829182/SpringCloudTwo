145ca17e12098605ee9eb77646b640cb0cecf52b
jack <wj903829182@sina.cn>
2017-10-10 21:01:56 +0800

    spring boot mongodb

---
+++ springboot9mongodb/src/main/java/com/jack/springboot9mongodb/Springboot9mongodbApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot9mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot9mongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot9mongodbApplication.class, args);
+	}
+}
---
+++ springboot9mongodb/src/test/java/com/jack/springboot9mongodb/Springboot9mongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.springboot9mongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot9mongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
