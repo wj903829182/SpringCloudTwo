86bd1409fa294067a87a0fe9ac84e078c2494a81
jack <wj903829182@sina.cn>
2017-10-10 23:17:17 +0800

    spring boot redis

---
+++ springboot10redis/src/main/java/com/jack/springboot10redis/Springboot10redisApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot10redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot10redisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot10redisApplication.class, args);
+	}
+}
