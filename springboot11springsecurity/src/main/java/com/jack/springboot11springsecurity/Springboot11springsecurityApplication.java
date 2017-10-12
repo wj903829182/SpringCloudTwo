077fdefa98ef7466c59e3a5df5b570b3b0070e11
jack <wj903829182@sina.cn>
2017-10-12 21:46:23 +0800

    spring boot security

---
+++ springboot11springsecurity/src/main/java/com/jack/springboot11springsecurity/Springboot11springsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot11springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11springsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot11springsecurityApplication.class, args);
+	}
+}
---
+++ springboot11springsecurity/src/test/java/com/jack/springboot11springsecurity/Springboot11springsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.springboot11springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot11springsecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
