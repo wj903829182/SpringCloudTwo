36432c072a135526629d8e7f4fb18f4d787c529d
jack <wj903829182@sina.cn>
2017-10-20 21:12:30 +0800

    spring boot transaction

--- springboot12springsecurit/src/main/java/com/jack/springboot12springsecurit/entity/SysRole.java
+++ springboot12springsecurit/src/main/java/com/jack/springboot12springsecurit/entity/SysRole.java
@@ -2,7 +2,6 @@ package com.jack.springboot12springsecurit.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
