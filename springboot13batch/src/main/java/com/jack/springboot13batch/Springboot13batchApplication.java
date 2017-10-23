6fdbed751eb3bb1b8e531561f297f2dd415f9608
jack <wj903829182@sina.cn>
2017-10-23 21:27:48 +0800

    spring boot transaction

---
+++ springboot13batch/src/main/java/com/jack/springboot13batch/Springboot13batchApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot13batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot13batchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot13batchApplication.class, args);
+	}
+}
