9458870aec6035624653b9a22a7b45c4a6fe1287
jack <wj903829182@sina.cn>
2017-11-08 23:15:55 +0800

    spring boot ssh

---
+++ springboot21ssh/src/main/java/com/jack/springboot21ssh/Springboot21sshApplication.java
@@ -0,0 +1,12 @@
+package com.jack.springboot21ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot21sshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot21sshApplication.class, args);
+	}
+}
---
+++ springboot21ssh/src/test/java/com/jack/springboot21ssh/Springboot21sshApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jack.springboot21ssh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot21sshApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
